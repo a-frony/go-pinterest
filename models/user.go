e05411ab2b3d6fcdcb4214911f31ffa73a663db4
Iggy Zuk <IgnatusZuk@gmail.com>
2019-10-31 21:37:37 +0000

    Pinterest API was slightly changed.

--- models/user.go
+++ models/user.go
@@ -4,21 +4,19 @@ import (
 	"github.com/BrandonRomano/iso8601"
 )
 
-const USER_FIELDS = "first_name,last_name,url,account_type,bio,counts,created_at,id,image,username"
+const USER_FIELDS = "id,username,first_name,last_name,bio,created_at,counts,image"
 
 // User is a struct that represents an individual user
 // from the Pinterest API.
 type User struct {
-	Id          string       `json:"id"`
-	Username    string       `json:"username"`
-	FirstName   string       `json:"first_name"`
-	LastName    string       `json:"last_name"`
-	Bio         string       `json:"bio"`
-	AccountType string       `json:"account_type"`
-	Url         string       `json:"account_type"`
-	CreatedAt   iso8601.Time `json:"created_at"`
-	Counts      UserCounts   `json:"counts"`
-	Image       Images       `json:"image"`
+	Id        string       `json:"id"`
+	Username  string       `json:"username"`
+	FirstName string       `json:"first_name"`
+	LastName  string       `json:"last_name"`
+	Bio       string       `json:"bio"`
+	CreatedAt iso8601.Time `json:"created_at"`
+	Counts    UserCounts   `json:"counts"`
+	Image     Images       `json:"image"`
 }
 
 type UserCounts struct {
